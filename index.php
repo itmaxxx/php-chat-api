c16d0a582ef7330c9519060b206c94dd9afce8fc
itmaxxx <dmitrievmaks77@gmail.com>
2022-01-24 05:30:39 +0200

    Fix cors errors

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
   @include_once "./utils/httpException.php";
   @include_once "./app/app.controller.php";
   
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+  
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return 0;
+    exit;
+  }
+  
   # Disable errors, should be false in production
   # error_reporting(false);
   
