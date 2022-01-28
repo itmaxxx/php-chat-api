54fb0ce3e1b31c2cb709f390fc6b655706a52262
itmaxxx <dmitrievmaks77@gmail.com>
2022-01-28 05:17:43 +0200

    Improve fixtures

--- fixtures/chats.php
+++ fixtures/chats.php
@@ -4,13 +4,15 @@
     "id" => "chat000000000001",
     "name" => "First private chat",
     "isPrivate" => 1,
+    "image" => "https://memepedia.ru/wp-content/uploads/2019/01/hamster-768x432.jpg",
     "inviteLink" => "invitelink000001"
   ];
   
   $GymPartyPublicChat = [
     "id" => "chat000000000002",
-    "name" => "Welcome to the club buddy",
+    "name" => "Gym chat",
     "isPrivate" => 0,
+    "image" => "https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/Gold%27s_Gym_logo.svg/1200px-Gold%27s_Gym_logo.svg.png",
     "inviteLink" => "invitelink000002"
   ];
   
--- fixtures/users.php
+++ fixtures/users.php
@@ -5,18 +5,24 @@
   $MaxDmitriev = [
     "id" => "user000000000001",
     "username" => "dmitriev",
+    "fullname" => "Max Dmitriev",
+    "profileImage" => "https://avatars.githubusercontent.com/u/38819868?v=4",
     "password" => $plainPassword
   ];
   
   $IlyaMehof = [
     "id" => "user000000000002",
     "username" => "mehoff",
+    "fullname" => "Ilya Mihov",
+    "profileImage" => "https://avatars.githubusercontent.com/u/44639162?v=4",
     "password" => $plainPassword
   ];
   
   $MatveyGorelik = [
     "id" => "user000000000003",
     "username" => "offiza",
+    "fullname" => "Matvey Gorelik",
+    "profileImage" => "https://avatars.githubusercontent.com/u/54431366?v=4",
     "password" => $plainPassword
   ];
   
